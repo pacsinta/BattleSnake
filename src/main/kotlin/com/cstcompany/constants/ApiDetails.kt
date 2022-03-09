75c26456da75b84a30f20f428f5533afbe8a27f0
Patrik Csikós <pacsinta@gmail.com>
2022-03-09 22:15:47 +0100

    ApiDetails fixed api version (int->string)

--- src/main/kotlin/com/cstcompany/constants/ApiDetails.kt
+++ src/main/kotlin/com/cstcompany/constants/ApiDetails.kt
@@ -4,10 +4,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ApiDetails(
-    val apiVersion: Int = 1,
+    val apiVersion: String = "1",
     val author: String = "Patrik Csikós",
     val color: String = "#888888",
     val head: String = "replit-mark",
     val tail: String = "rbc-necktie",
-    val version: String = "0.0.1-beta"
+    val version: String? = null
 )
