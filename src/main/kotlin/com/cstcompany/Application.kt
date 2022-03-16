560021b9b659be14c49f3821397649d5089bb166
Patrik Csikos <pacsinta@gmail.com>
2022-03-16 17:04:55 +0100

    https fixed

--- src/main/kotlin/com/cstcompany/Application.kt
+++ src/main/kotlin/com/cstcompany/Application.kt
@@ -1,24 +1,27 @@
 package com.cstcompany
 
-import alias
+import com.cstcompany.plugins.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.cstcompany.plugins.*
-import io.ktor.http.ContentDisposition.Companion.File
-import io.ktor.network.tls.certificates.*
-import io.ktor.network.tls.extensions.*
-import jksPassword
-import keyPassword
 import java.io.File
+import java.io.FileInputStream
+import java.security.KeyStore
+
 
 fun main() {
-    val keyStoreFile = File("~/keystore.jks")
-    val keystore = generateCertificate(
-        file = keyStoreFile,
-        keyAlias = alias,
-        keyPassword = keyPassword,
-        jksPassword = jksPassword
-    )
+    val ks = KeyStore.getInstance("JKS")
+
+
+    val pwd = File("pwd")
+
+    val passwords = pwd.readText().split(';')
+    val keyPassword = passwords[0]
+    val jksPassword = passwords[1]
+    val alias = passwords[2]
+
+    FileInputStream("keystore.jks").use { file->
+        ks.load(file, keyPassword.toCharArray())
+    }
 
     val environment = applicationEngineEnvironment {
         module {
@@ -27,17 +30,14 @@ fun main() {
         }
 
         sslConnector(
-            keyStore = keystore,
+            keyStore = ks,
             keyAlias = alias,
             keyStorePassword = {keyPassword.toCharArray()},
             privateKeyPassword = {jksPassword.toCharArray()},
         ){
             port = 4000
-            keyStorePath = keyStoreFile
         }
     }
 
-    embeddedServer(Netty, environment = environment) {
-
-    }.start(wait = true)
+    embeddedServer(Netty, environment = environment).start(wait = true)
 }
